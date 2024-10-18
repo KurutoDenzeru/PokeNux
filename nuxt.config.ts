68af6980b3039205549b8ca0457764934ad71ce4
kurutodenzeru <kurtdenzel51@gmail.com>
2024-10-18 17:48:17 +0800

    build: added nuxt seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,21 @@ export default defineNuxtConfig({
 	compatibilityDate: "2024-04-03",
 	devtools: { enabled: true },
 	css: ["~/assets/css/main.css"],
+
 	postcss: {
 		plugins: {
 			tailwindcss: {},
 			autoprefixer: {},
 		},
+	},
+
+	modules: ["@nuxtjs/seo"],
+
+	site: {
+		url: "https://pokenuxt.nuxt.dev/",
+		name: "PokeNuxt",
+		description:
+			"Sleek and responsive Pokédex built with Nuxt,js, styled using Tailwind CSS, and powered by PokéAPI. It offers an intuitive interface for exploring detailed Pokémon information effortlessly.Welcome to my awesome site!",
+		defaultLocale: "en", // not needed if you have @nuxtjs/i18n installed
 	},
 });
