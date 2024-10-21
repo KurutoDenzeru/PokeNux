5ce42559d9b2d7dcedd4ef83bc6f2f915aba3ca4
kurutodenzeru <kurtdenzel51@gmail.com>
2024-10-21 11:04:24 +0800

    docs: revert back to no nuxt seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,24 +10,4 @@ export default defineNuxtConfig({
 			autoprefixer: {},
 		},
 	},
-
-	modules: [
-		"@nuxtjs/seo",
-		"@nuxtjs/robots",
-		"@nuxtjs/sitemap",
-		"nuxt-link-checker",
-	],
-
-	site: {
-		url: "https://pokenuxt.nuxt.dev/",
-		name: "PokeNuxt",
-		description:
-			"Sleek and responsive Pokédex built with Nuxt,js, styled using Tailwind CSS, and powered by PokéAPI. It offers an intuitive interface for exploring detailed Pokémon information effortlessly.Welcome to my awesome site!",
-		defaultLocale: "en", // not needed if you have @nuxtjs/i18n installed
-	},
-
-	robots: {
-		...(process.env.NODE_ENV === "production" ? { indexable: true } : {}),
-		sitemap: "https://pokenuxt.nuxt.dev/sitemap.xml",
-	},
 });
