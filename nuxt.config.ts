6fd083f2dae8ccb835ff95060cc2f8568fc66492
kurutodenzeru <kurtdenzel51@gmail.com>
2024-10-21 13:27:46 +0800

    build: added nuxtjs/tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	compatibilityDate: "2024-04-03",
-	devtools: { enabled: true },
-	css: ["~/assets/css/main.css"],
+	devtools: { enabled: false },
+	// css: ["~/assets/css/main.css"],
 
 	postcss: {
 		plugins: {
@@ -10,4 +10,17 @@ export default defineNuxtConfig({
 			autoprefixer: {},
 		},
 	},
+
+	app: {
+		pageTransition: {
+			name: "fade",
+			mode: "out-in", // default
+		},
+		layoutTransition: {
+			name: "slide",
+			mode: "out-in", // default
+		},
+	},
+
+	modules: ["@nuxtjs/tailwindcss"],
 });
