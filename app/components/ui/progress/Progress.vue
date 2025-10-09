cbd60faf6cf96238625dd341231c90f4b379a396
KurutoDenzeru <kurtdenzel51@gmail.com>
2025-10-09 02:58:50 -0700

    refactor: clean up imports and formatting in Progress component for improved readability

--- app/components/ui/progress/Progress.vue
+++ app/components/ui/progress/Progress.vue
@@ -1,39 +1,32 @@
 <script setup lang="ts">
-import type { ProgressRootProps } from "reka-ui"
-import type { HTMLAttributes } from "vue"
-import { reactiveOmit } from "@vueuse/core"
-import {
-  ProgressIndicator,
-  ProgressRoot,
+  import type { ProgressRootProps } from "reka-ui"
+  import type { HTMLAttributes } from "vue"
+  import { reactiveOmit } from "@vueuse/core"
+  import {
+    ProgressIndicator,
+    ProgressRoot,
 
-} from "reka-ui"
-import { cn } from '~/lib/utils'
+  } from "reka-ui"
+  import { cn } from '~/lib/utils'
 
-const props = withDefaults(
-  defineProps<ProgressRootProps & { class?: HTMLAttributes["class"] }>(),
-  {
-    modelValue: 0,
-  },
-)
+  const props = withDefaults(
+    defineProps<ProgressRootProps & { class?: HTMLAttributes["class"] }>(),
+    {
+      modelValue: 0,
+    },
+  )
 
-const delegatedProps = reactiveOmit(props, "class")
+  const delegatedProps = reactiveOmit(props, "class")
 </script>
 
 <template>
-  <ProgressRoot
-    data-slot="progress"
-    v-bind="delegatedProps"
-    :class="
-      cn(
-        'bg-emerald-100 dark:bg-emerald-900/20 relative h-2 w-full overflow-hidden rounded-full',
-        props.class,
-      )
-    "
-  >
-    <ProgressIndicator
-      data-slot="progress-indicator"
+  <ProgressRoot data-slot="progress" v-bind="delegatedProps" :class="cn(
+    'bg-emerald-100 dark:bg-emerald-900/20 relative h-2 w-full overflow-hidden rounded-full',
+    props.class,
+  )
+    ">
+    <ProgressIndicator data-slot="progress-indicator"
       class="bg-emerald-600 dark:bg-emerald-400 h-full w-full flex-1 transition-all"
-      :style="`transform: translateX(-${100 - (props.modelValue ?? 0)}%);`"
-    />
+      :style="`transform: translateX(-${100 - (props.modelValue ?? 0)}%);`" />
   </ProgressRoot>
 </template>
